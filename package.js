24af806887dc69f1e499dea68b99b652390db3d0
Leonidez Acosta <leonidez@poeticsystems.com>
2015-06-09 12:29:40 -0500

    initial commit and dependencies

---
+++ package.js
@@ -0,0 +1,34 @@
+Package.describe({
+  name: 'poetic:swiper-slides',
+  version: '0.0.1',
+  summary: 'an implementation Swiper combined with Iron Router query param management',
+  git: '',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.2');
+
+  api.use([
+    'templating',
+    'iron:router@1.0.9',
+    'cottz:iron-query@1.2.3',
+    'lacosta:swiper@0.0.1'
+  ]);
+
+  api.addFiles([
+    'swiper-slides.html',
+    'swiper-slides.js'
+  ], [
+    'client'
+  ]);
+
+  api.imply([
+    'iron:router@1.0.9',
+    'cottz:iron-query@1.2.3'
+  ]);
+
+  api.export('Swiper');
+});
+
+Package.onTest(function(){});
